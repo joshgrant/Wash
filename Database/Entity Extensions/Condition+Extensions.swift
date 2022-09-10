21eab67f04428bc28fe9d8639193aac6991492b1
Joshua Grant <joshgrant@pm.me>
2022-09-09 19:35:09 -0600

    Removing extra file

--- Database/Entity Extensions/Condition+Extensions.swift
+++ Database/Entity Extensions/Condition+Extensions.swift
@@ -66,6 +66,42 @@ Right hand:         \(rightHand?.description ?? "nil")
     }
 }
 
+extension Condition
+{
+    func setComparison(_ comparison: ComparisonType, type: String)
+    {
+        booleanComparisonType = nil
+        numberComparisonType = nil
+        dateComparisonType = nil
+
+        switch comparison
+        {
+        case .boolean:
+            guard let booleanType = BooleanComparisonType(string: type) else
+            {
+                print("Invalid boolean type.")
+                return
+            }
+            booleanComparisonType = booleanType
+        case .number:
+            guard let numberType = NumberComparisonType(string: type) else
+            {
+                print("Invalid number type/")
+                return
+            }
+            numberComparisonType = numberType
+        case .date:
+            guard let dateType = DateComparisonType(string: type) else
+            {
+                print("Invalid date type.")
+                return
+            }
+            dateComparisonType = dateType
+        }
+    }
+}
+
+
 public extension Condition
 {
     var comparisonType: ComparisonType?
